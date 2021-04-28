863b3a17689866e31ab725fd962005b348d30c6e
Wenqi He <hewenqi96@gmail.com>
2021-04-28 23:11:08 +0800

    :tada: forked && refactored a little

--- server.py
+++ server.py
@@ -2,124 +2,75 @@ import logging
 import select
 import socket
 import struct
-from socketserver import ThreadingMixIn, TCPServer, StreamRequestHandler
+from socketserver import ThreadingTCPServer, StreamRequestHandler
 
 logging.basicConfig(level=logging.DEBUG)
 SOCKS_VERSION = 5
-
-
-class ThreadingTCPServer(ThreadingMixIn, TCPServer):
-    pass
-
+NO_AUTHENTICATION_REQUIRED = 0
+IPV4 = 1
+IPV6 = 4
+DOMAINNAME = 3
+CONNECT = 1
+SUCCESS = 0
+CONNECTION_REFUSED = 5
 
 class SocksProxy(StreamRequestHandler):
-    username = 'username'
-    password = 'password'
-
     def handle(self):
-        logging.info('Accepting connection from %s:%s' % self.client_address)
+        ip, port = self.client_address
+        logging.info(f'Accepting connection from {ip}:{port}')
 
-        # greeting header
-        # read and unpack 2 bytes from a client
-        header = self.connection.recv(2)
-        version, nmethods = struct.unpack("!BB", header)
-
-        # socks 5
+	    # greeting
+        version, nmethods = struct.unpack("!BB", self.connection.recv(2))
+        methods = [ord(self.connection.recv(1)) for _ in range(nmethods)]
         assert version == SOCKS_VERSION
-        assert nmethods > 0
+        assert NO_AUTHENTICATION_REQUIRED in methods
+        self.connection.sendall(struct.pack("!BB", SOCKS_VERSION, 0))
 
-        # get available methods
-        methods = self.get_available_methods(nmethods)
-
-        # accept only USERNAME/PASSWORD auth
-        if 2 not in set(methods):
-            # close connection
-            self.server.close_request(self.request)
-            return
-
-        # send welcome message
-        self.connection.sendall(struct.pack("!BB", SOCKS_VERSION, 2))
-
-        if not self.verify_credentials():
-            return
-
-        # request
+        # try connecting to remote
         version, cmd, _, address_type = struct.unpack("!BBBB", self.connection.recv(4))
         assert version == SOCKS_VERSION
 
-        if address_type == 1:  # IPv4
+        if address_type == IPV4:
             address = socket.inet_ntoa(self.connection.recv(4))
-        elif address_type == 3:  # Domain name
-            domain_length = self.connection.recv(1)[0]
-            address = self.connection.recv(domain_length)
-            address = socket.gethostbyname(address)
+        elif address_type == DOMAINNAME:
+            domain_length = ord(self.connection.recv(1))
+            domain_name = self.connection.recv(domain_length).decode('utf-8')
+            address = socket.gethostbyname(domain_name)
+            logging.info(f'Resolved {domain_name} to {address}')
         port = struct.unpack('!H', self.connection.recv(2))[0]
 
-        # reply
         try:
-            if cmd == 1:  # CONNECT
+            if cmd == CONNECT:
                 remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 remote.connect((address, port))
-                bind_address = remote.getsockname()
-                logging.info('Connected to %s %s' % (address, port))
+                logging.info(f'Connected to {address}:{port}')
             else:
+                logging.error(f'Command {cmd} not implemented!')
                 self.server.close_request(self.request)
+                return
+            bound_addr, bound_port = remote.getsockname()
+            bound_addr, = struct.unpack('!I', socket.inet_aton(bound_addr))
 
-            addr = struct.unpack("!I", socket.inet_aton(bind_address[0]))[0]
-            port = bind_address[1]
-            reply = struct.pack("!BBBBIH", SOCKS_VERSION, 0, 0, 1,
-                                addr, port)
+            # success
+            reply = struct.pack("!BBBBIH", SOCKS_VERSION, SUCCESS, 0, IPV4, bound_addr, bound_port)
 
         except Exception as err:
             logging.error(err)
-            # return connection refused error
-            reply = self.generate_failed_reply(address_type, 5)
+
+            # failure
+            reply = struct.pack("!BBBBIH", SOCKS_VERSION, CONNECTION_REFUSED, 0, address_type, 0, 0)
 
         self.connection.sendall(reply)
 
-        # establish data exchange
-        if reply[1] == 0 and cmd == 1:
-            self.exchange_loop(self.connection, remote)
-
+        # connection established
+        if cmd == CONNECT and reply[1] == SUCCESS:
+            self.loop(self.connection, remote)
         self.server.close_request(self.request)
 
-    def get_available_methods(self, n):
-        methods = []
-        for i in range(n):
-            methods.append(ord(self.connection.recv(1)))
-        return methods
 
-    def verify_credentials(self):
-        version = ord(self.connection.recv(1))
-        assert version == 1
-
-        username_len = ord(self.connection.recv(1))
-        username = self.connection.recv(username_len).decode('utf-8')
-
-        password_len = ord(self.connection.recv(1))
-        password = self.connection.recv(password_len).decode('utf-8')
-
-        if username == self.username and password == self.password:
-            # success, status = 0
-            response = struct.pack("!BB", version, 0)
-            self.connection.sendall(response)
-            return True
-
-        # failure, status != 0
-        response = struct.pack("!BB", version, 0xFF)
-        self.connection.sendall(response)
-        self.server.close_request(self.request)
-        return False
-
-    def generate_failed_reply(self, address_type, error_number):
-        return struct.pack("!BBBBIH", SOCKS_VERSION, error_number, 0, address_type, 0, 0)
-
-    def exchange_loop(self, client, remote):
-
+    def loop(self, client, remote):
         while True:
-
-            # wait until client or remote is available for read
-            r, w, e = select.select([client, remote], [], [])
+            r, _, _ = select.select([client, remote], [], [])
 
             if client in r:
                 data = client.recv(4096)
@@ -131,7 +82,6 @@ class SocksProxy(StreamRequestHandler):
                 if client.send(data) <= 0:
                     break
 
-
 if __name__ == '__main__':
-    with ThreadingTCPServer(('127.0.0.1', 9011), SocksProxy) as server:
+    with ThreadingTCPServer(('127.0.0.1', 1080), SocksProxy) as server:
         server.serve_forever()
